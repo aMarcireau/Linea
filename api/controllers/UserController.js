6eae63c995795dd27d07bd1d33da3a1ebae36241
Alexandre Marcireau <alexandre.marcireau@student.ecp.fr>
2015-01-12 11:58:02 +0100

    Add user login

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -6,4 +6,36 @@
  */
 
 module.exports = {
+    /**
+     * Login as user
+     */
+    login: function(req, res) {
+        var bcrypt = require('bcrypt');
+
+        User.findOneByEmail(req.params('email')).done(function (err, user) {
+            if (err) { 
+                res.json({error: 'database error'}, 500);
+            }
+
+            if (user) {
+                bcrypt.compare(req.body.password, user.password, function (err, match) {
+                    if (err) { 
+                        res.json({ error: 'server error' }, 500);
+                    }
+
+                    if (match) {
+                        req.session.user = user.email;
+                        res.json(user);
+                    } else {
+                        if (req.session.user) {
+                            req.session.user = null;
+                        }
+                        res.json({ error: 'invalid password' }, 400);
+                    }
+                });
+            } else {
+                res.json({ error: 'User not found' }, 404);
+            }
+        });
+    }
 };
